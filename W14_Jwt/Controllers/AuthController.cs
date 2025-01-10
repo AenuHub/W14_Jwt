3aa33d1d21284342f084789c936fc61674ea298e
AenuHub <jesuskane92@gmail.com>
2025-01-10 12:38:22 +0300

    create AuthController.cs

--- W14_Jwt/Controllers/AuthController.cs
+++ W14_Jwt/Controllers/AuthController.cs
@@ -1,12 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using W14_Jwt.Data;
+using W14_Jwt.Dtos;
 
 namespace W14_Jwt.Controllers;
 
+[Route("api/[controller]")]
+[ApiController]
 public class AuthController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly UserDbContext _context;
+    private readonly IConfiguration _config;
+    
+    public AuthController(UserDbContext context, IConfiguration config)
     {
-        return View();
+        _context = context;
+        _config = config;
+    }
+    
+    [HttpPost("login")]
+    [Authorize]
+    public IActionResult Login(LoginDto loginDto)
+    {
+        var user = _context.Users.FirstOrDefault(u => u.Email == loginDto.Email && u.Password == loginDto.Password);
+        
+        if (user == null)
+        {
+            return BadRequest("Invalid email or password");
+        }
+        
+        var claims = new Claim[]
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+        
+        var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_config["Jwt:Key"]!));
+        var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+        var token = new JwtSecurityToken(
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.UtcNow.AddMinutes(30),
+            signingCredentials: creds
+        );
+        
+        return Ok(new
+        {
+            token = new JwtSecurityTokenHandler().WriteToken(token),
+            expiration = token.ValidTo
+        });
     }
 }
